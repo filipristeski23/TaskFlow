70d0a198d0a11c3bf53f9eb475ed2b61b054bc89
Filip <filipristeski23@gmail.com>
2024-09-22 21:36:41 +0200

    Finished working on the motivation component

--- frontend/src/components/Motivation.jsx
+++ frontend/src/components/Motivation.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Div = styled.div`
   width: 100%;
@@ -20,13 +22,35 @@ const H3 = styled.h3`
   display: flex;
   justify-content: center;
   align-items: center;
+  padding-left: 2rem;
+  text-align: center;
 `;
 
 function Motivation() {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    fetch("https://gomezmig03.github.io/MotivationalAPI/en.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Unable to retrieve data from the server.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const phrase = data[randomIndex].phrase;
+        setQuote(phrase);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
     <Div>
       <DivWrapper>
-        <H3>The motivation goes here</H3>
+        <H3>{quote}</H3>
       </DivWrapper>
     </Div>
   );
